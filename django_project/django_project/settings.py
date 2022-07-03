a54c91098c6ea2d08efe6a5e65a73e7079d3fab5
mrblanc <paulwilson1337@gmail.com>
2022-07-03 15:32:10 -0400

    Add user login page
    Can now login and logout without going through the admin page.
    Currently just displays a page with the logged in username, or a prompt
    to login.
    
    Once products have been added, we can display them when a user is logged
    in.

--- django_project/django_project/settings.py
+++ django_project/django_project/settings.py
@@ -27,6 +27,7 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
+AUTH_USER_MODEL = 'storefront.User'
 
 # Application definition
 
@@ -54,7 +55,7 @@ ROOT_URLCONF = 'django_project.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR , "templates"],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -66,6 +67,9 @@ TEMPLATES = [
         },
     },
 ]
+
+LOGIN_REDIRECT_URL = "home"
+LOGOUT_REDIRECT_URL = "home"
 
 WSGI_APPLICATION = 'django_project.wsgi.application'
 
